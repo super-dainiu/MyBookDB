2ff789d7f7931c3bca45a1b29f6a0ef9effc15cd
Super-Dainiu <19307110036@fudan.edu.cn>
2021-05-23 18:42:14 +0800

    sdadsa

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-path("", views.index, name="index"),
-path("v1/", views.v1, name="v1")
+path("<int:id>", views.index, name="in"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-	return HttpResponse("<h1> I am Daniel </h1>")
-	
-def v1(response):
-	return HttpResponse("<h2> Hello </h2?")
+def index(response, id):
+	ls = ToDoList.object.get(id=id)
+	return HttpResponse("<h1> %s </h1>" % ls.name)
