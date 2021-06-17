7450dce2ab9e21156ed2f2ff1992c12414ea3aff
Super Daniel <78588128+Super-Dainiu@users.noreply.github.com>
2021-06-18 00:50:20 +0800

    data.py 6/18

--- books/views.py
+++ books/views.py
@@ -1,14 +1,64 @@
-from django.shortcuts import render, redirect
-from login.models import User as user
-from .models import *
 from django.db import transaction
+from django.shortcuts import render, redirect
+
+from login.models import User
+from publishers.models import Publishers
+from writers.models import Writers
+from .models import *
+
 
 # Create your views here.
 
 
 def index(request):
-
-    return render(request, 'books.html')
+    info = {"fields": Books._meta.fields, "books": {}}
+    if not (User.objects.filter(ip=request.META['REMOTE_ADDR'])):
+        return redirect("../")
+    title_filter = ''
+    id_filter = ''
+    orderby = ''
+    orderby2 = ''
+    order = ''
+    order2 = ''
+    delete_id = None
+    if request.method == "GET":
+        title_filter = request.GET.get('title') if request.GET.get('title') else ''
+        writer_filter = request.GET.get('writer') if request.GET.get('writer') else ''
+        publisher_filter = request.GET.get('publisher') if request.GET.get('publisher') else ''
+        id_filter = request.GET.get('id') if request.GET.get('id') else ''
+        classification_filter = request.GET.get('classification') if request.GET.get('classification') else ''
+        orderby = request.GET.get('orderby')
+        order = request.GET.get('order')
+        orderby2 = request.GET.get('orderby2')
+        order2 = request.GET.get('order2')
+        delete_id = request.GET.get('delete')
+        info.update(
+            {"title_filter": title_filter, "writer_filter": writer_filter, "orderby": orderby, "order": order,
+             "classification_filter": classification_filter,
+             "orderby2": orderby2, "order2": order2, "publisher_filter": publisher_filter, "id_filter": id_filter})
+    if delete_id:
+        Books.objects.filter(id=delete_id).delete()
+    orders = []
+    if orderby:
+        if order == "asc":
+            orders.append(orderby)
+        if order == "desc":
+            orders.append('-' + orderby)
+    if orderby2:
+        if order2 == "asc":
+            orders.append(orderby2)
+        if order2 == "desc":
+            orders.append('-' + orderby2)
+    if id_filter:
+        info.update({"books": Books.objects.filter(
+            id=int(id_filter, title__icontains=title_filter, publishers__name_icontains=publisher_filter,
+                   writers__name__icontains=writer_filter)).order_by(*orders)})
+    else:
+        info.update({"books": Books.objects.filter(title__icontains=title_filter,
+                                                   publishers__name__icontains=publisher_filter,
+                                                   writers__name__icontains=writer_filter).order_by(*orders)})
+    print(info)
+    return render(request, 'books.html', info)
 
 
 def create(request):
@@ -44,8 +94,8 @@ def create(request):
 def edit(request, bookid):
     with transaction.atomic():
         target = Books.objects.select_for_update(skip_locked=True).get(id=bookid)
-        info = {'name': target.name, 'sex': target.sex, 'phone': target.phone, 'email': target.email,
-                'address': target.address, 'vip': str(target.vip)}
+        info = {'title': target.title, 'sex': target.sex, 'phone': target.phone, 'email': target.email,
+                'writer': target.writer, 'vip': str(target.vip)}
         if request.method == "POST":
             title = request.POST.get("title")
             writers = request.POST.get("writers")
@@ -59,8 +109,9 @@ def edit(request, bookid):
             storage = request.POST.get("storage")
             info.update(
                 {'title': title, 'writers': writers, 'price': price, 'price_vip': price_vip, 'publishers': publishers,
-             'classification': classification, 'sub_classification': sub_classification, 'publish_date': publish_date,
-             'edition': edition, 'storage': storage})
+                 'classification': classification, 'sub_classification': sub_classification,
+                 'publish_date': publish_date,
+                 'edition': edition, 'storage': storage})
 
             if Books.objects.filter(title=title) & Books.objects.filter(publishers=publishers) & Books.objects.filter(
                     edition=edition):
