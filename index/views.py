2af0282928d2bfbd351e3d7b7f105e7366a4db6e
Super Daniel <78588128+Super-Dainiu@users.noreply.github.com>
2021-06-23 21:38:14 +0800

    Orders Finished

--- index/views.py
+++ index/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from login.models import User
 from users.models import User as Customer
 from books.models import Books
-from orders.models import Orders
+from orders.models import Orders, Details
+import random, time, datetime
 from django.utils import timezone
 
 # Create your views here.
@@ -10,14 +11,35 @@ from django.utils import timezone
 
 def index(request):
     href = 'home/' if User.objects.filter(ip=request.META['REMOTE_ADDR']) else 'accounts/login'
-    print(request.META['REMOTE_ADDR'])
     return render(request, 'index.html', {"href": href})
 
 
 def home(request):
+    info = {}
     if User.objects.filter(ip=request.META['REMOTE_ADDR']):
         if request.method == "GET":
-            pass
-        return render(request, 'home.html')
+            if request.GET.get("business") == "1":
+                num = random.randint(5, 10)
+                for i in range(num):
+                    init_time = ''.join(str(time.time()).split('.'))
+                    time.sleep(0.01)
+                    user = random.choice(list(Customer.objects.all()))
+                    new, created = Orders.objects.get_or_create(id=init_time,
+                                                                date=timezone.now(),
+                                                                user=user,
+                                                                confirm='0',
+                                                                )
+                    for j in range(random.randint(1, 5)):
+                        book = random.choice(list(Books.objects.all()))
+                        if user.vip == 1:
+                            bookprice = book.price_vip
+                        else:
+                            bookprice = book.price
+                        Details.objects.create(book=book,
+                                               order=new,
+                                               price=bookprice,
+                                               count=random.randint(1, 20))
+                info.update({"message": "%d more new orders are coming!" % num})
+        return render(request, 'home.html', info)
     else:
         return redirect("../")
